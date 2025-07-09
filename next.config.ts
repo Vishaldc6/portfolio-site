5933055466d9db970547c4cd8c01c4c457bdc7a6
DREAMWORLD <dreamworld@DREAMWORLDs-iMac-Pro.local>
2025-07-10 00:16:34 +0530

    Change App title

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
 
+//https://portfolio-site-gamma-steel.vercel.app/
+
 const nextConfig: NextConfig = {
   /* config options here */  
    images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'via.placeholder.com',
+        hostname: 'portfolio-site-gamma-steel.vercel.app',
         port: '',
         pathname: '/**',
       },
